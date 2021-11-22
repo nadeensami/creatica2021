e9bd8afe3be6be5e1b73d99645bb229026f39a30
Nadeen <nadeen@ualberta.ca>
2021-11-21 17:40:37 -0700

    pushing changes to clear local directory

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeScreen from "./components/homeScreen";
 import MainScreen from "./components/main";
+import "./style/app.css";
 
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route path="/main">
-					<MainScreen />
-				</Route>
-				<Route path="/">
-					<HomeScreen />
-				</Route>
-			</Switch>
+			<div className="app">
+				<Switch>
+					<Route path="/main">
+						<MainScreen />
+					</Route>
+					<Route path="/">
+						<HomeScreen />
+					</Route>
+				</Switch>
+			</div>
 		</Router>
 	);
 }
